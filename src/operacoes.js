69394ae3cfd6a453fdf58421ef11bcf240dcf328
helena meirelles <meirellesahelena@gmail.com>
2023-02-13 17:13:59 -0300

    operação sub e soma

---
+++ src/operacoes.js
@@ -0,0 +1,18 @@
+function sum(num1, num2) {
+    if(isNaN(num1) || isNaN(num2)) {
+        throw 'nao e um numero'
+    }
+    return num1 + num2
+}
+
+function sub(num1, num2) {
+    if(isNaN(num1) || isNaN(num2)) {
+        throw 'nao e um numero'
+    }
+    return num1 - num2
+}
+module.exports ={
+    sum,
+    sub
+}
+
---
+++ test/operacao.test.js
@@ -0,0 +1,42 @@
+const { describe, expect, it } = require('@jest/globals')
+const calculadora = require('../src/operacoes')
+
+describe('teste da calculadora', () => {
+
+    it('operação de soma', () => {
+        let resultado = calculadora.sum(1, 2);
+        expect(resultado).toEqual(3);
+
+        resultado = calculadora.sum(-1, 2);
+        expect(resultado).toEqual(1);
+
+        resultado = calculadora.sum(0, 2);
+        expect(resultado).toEqual(2);
+
+        resultado = () => calculadora.sum("a", 2);
+        expect(resultado).toThrow('nao e um numero');
+        
+        resultado = () => calculadora.sum(2, "a");
+        expect(resultado).toThrow('nao e um numero');
+
+
+    })
+
+    it('operação de sub', () => {
+        let resultado = calculadora.sub(1, 2);
+        expect(resultado).toEqual(-1);
+
+        resultado = calculadora.sub(3, 2);
+        expect(resultado).toEqual(-1);
+
+        resultado = calculadora.sub(2, 0);
+        expect(resultado).toEqual(-2);
+
+        resultado = () => calculadora.sub("a", 2);
+        expect(resultado).toThrow('nao e um numero');
+
+        resultado = () => calculadora.sub(2, "a");
+        expect(resultado).toThrow('nao e um numero');
+
+    })
+})
